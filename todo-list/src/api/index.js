485ea426a0584f9cf4b62da955afcc32e46408aa
Nelson Ubac <nelsonubac90@gmail.com>
2023-11-15 18:00:14 -0300

    sync api firebase and express with react

--- todo-list/src/api/index.js
+++ todo-list/src/api/index.js
@@ -1,41 +1,45 @@
 /* eslint-disable no-undef */
 const express = require('express');
-
-const app = express();
-
+const cors = require('cors');
 const admin = require("firebase-admin");
-
 var serviceAccount = require("./firebase-admin-sdk.json"); // Your own service account
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-  
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server working on port: ${port}`);
-  });
 
-  const db = admin.firestore();
+const app = express();
+app.use(cors()); // Habilitar CORS
+const bodyParser = require('body-parser');
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server working on port: ${port}`);
+});
+
+const db = admin.firestore();
 
 // Get information
 app.get("/data", async (req, res) => {
-    try {
-      const data = [];
-      const snapshot = await db.collection("todo-list").get();
-      
-      snapshot.forEach((doc) => {
-        data.push(doc.data());
-      });
-  
-      res.json(data);
-    } catch (error) {
-      console.error("Error obtaining data:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
+  try {
+    const data = [];
+    const snapshot = await db.collection("todo-list").get();
+    
+    snapshot.forEach((doc) => {
+      data.push(doc.data());
+    });
 
-// Add data
+    res.json(data);
+  } catch (error) {
+    console.error("Error obtaining data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.post("/AddData", async (req, res) => {
   const newData = req.body;
   await db.collection("todo-list").add(newData);
--- todo-list/src/components/ToDoList.jsx
+++ todo-list/src/components/ToDoList.jsx
@@ -1,56 +1,68 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function ToDoList() {
-  const [tasks, setTasks] = useState([]);
+	const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    const storedTasks = localStorage.getItem('tasks');
-    if (storedTasks) {
-      setTasks(JSON.parse(storedTasks));
-    }
-  }, []);
+	useEffect(() => {
+		fetch('http://localhost:3000/data', { mode: 'cors' })
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				return response.json();
+			})
+			.then((data) => {
+        setTasks(data);
+      }
+			)
+			.catch((error) => console.error('Error:', error));
+	}, []);
 
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
+  const addData = async (data) => {
+  try {
+    const response = await axios.post('http://localhost:3000/AddData', data);
+    console.log('Data added successfully:', response.data);
+  } catch (error) {
+    console.error('Error adding data:', error);
+  }
+};
 
-  const addTask = () => {
-    const input = document.querySelector('input');
-    const newTask = input.value;
-    setTasks([...tasks, newTask]);
-    input.value = '';
-  };
+const addTask = () => {
+  const newTask = document.querySelector('input').value;
+  addData({ name: newTask, id: tasks.length + 1 });
+};
 
-  const deleteTask = (index) => {
-    const newTaskList = [...tasks];
-    newTaskList.splice(index, 1);
-    setTasks(newTaskList);
-  };
+	const deleteTask = (index) => {
+		const newTaskList = [...tasks];
+		newTaskList.splice(index, 1);
+		setTasks(newTaskList);
+	};
 
-  return (
-    <div className='container'>
-      <img
-        src='https://i.ibb.co/fqBcxjn/con-fondo.png'
-        alt='logo Nelson Dev'
-      />
-      <h1>ToDo List</h1>
-      <input
-        type='text'
-        placeholder='Add a task'
-      />
-      <button onClick={addTask}>Add</button>
-      <ul>
-        {tasks.map((task, index) => {
-          return (
-            <li key={index}>
-              {task}
-              <button onClick={() => deleteTask(index)}>Delete</button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<img
+				src='https://i.ibb.co/fqBcxjn/con-fondo.png'
+				alt='logo Nelson Dev'
+			/>
+			<h1>ToDo List</h1>
+			<input
+				type='text'
+				placeholder='Add a task'
+			/>
+			<button onClick={addTask}>Add</button>
+			<ul>
+				{tasks.map((task, index) => {
+					return (
+						<li key={index}>
+							{task.name}
+							<button onClick={() => deleteTask(index)}>Delete</button>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default ToDoList;
